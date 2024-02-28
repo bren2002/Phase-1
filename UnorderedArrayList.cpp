9da0f80f7e842f7f79de59a1e4991d7728a67ddc
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-02-27 23:01:50 -0800

    Update UnorderedArrayList.cpp

--- UnorderedArrayList.cpp
+++ UnorderedArrayList.cpp
@@ -2,10 +2,9 @@
 // Brendan Scharmann, #825291656 
 // 2024 - 02 - 27
 #include "UnorderedArrayList.h"
-
 UnorderedArrayList::UnorderedArrayList() {
     // Initialize the array and other variables
-    // You can set initial values
+    // Set initial values
 }
 
 UnorderedArrayList::UnorderedArrayList(int initialCapacity) {
@@ -13,11 +12,54 @@ UnorderedArrayList::UnorderedArrayList(int initialCapacity) {
 }
 
 bool UnorderedArrayList::add(int data) {
-    // Implement the logic to add data to the unordered array
+    // Put in the logic to add data to the unordered array
     // Return true if correct, false otherwise
 }
 
 bool UnorderedArrayList::add(int index, int data) {
-    // Implement the logic to add data at the specific index
+    // Put in the logic to add data at the specific index
     // Return true if correct, false otherwise
+}
+
+void UnorderedArrayList::clear() {
+    // Implement the logic to clear the array
+}
+
+bool UnorderedArrayList::contains(int data) {
+    // Put in the logic to check if the array contains the specified data
+    // Return true if found, false otherwise
+}
+
+int UnorderedArrayList::get(int index) {
+    // Put in the logic to get the data at the specified index
+    // Return the data if index is correct, otherwise
+}
+
+int UnorderedArrayList::indexOf(int data) {
+    // Put in the logic to find the index of the specified data
+    // Return the index if found, -1 otherwise
+}
+
+bool UnorderedArrayList::isEmpty() {
+    // Put in the logic to check if the array is empty
+    // Return true if empty, false otherwise
+}
+
+int UnorderedArrayList::remove(int index) {
+    // Put in the logic to remove the data at the specified index
+    // Return the removed data if index is correct, otherwise
+}
+
+bool UnorderedArrayList::removeAll(int data) {
+    // Put in the logic to remove all the times of the specified data
+    // Return true if at least one time is removed, false otherwise
+}
+
+int UnorderedArrayList::size() {
+    // Put in the logic to get the size of the array
+    // Return the size
+}
+
+void UnorderedArrayList::trimToSize() {
+    // Put in the logic to trim the capacity to the current size
 }
