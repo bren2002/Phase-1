716dfb6d84ab8b73bc34d1b9d4fad5fbd526d4fc
bren2002 <161561650+bren2002@users.noreply.github.com>
2024-02-27 22:40:06 -0800

    Create UnorderedArrayList.h

---
+++ UnorderedArrayList.h
@@ -0,0 +1,23 @@
+// UnorderedArrayList.cpp
+// Brendan Scharmann, #825291656 
+// 2024 - 02 - 27
+#include "UnorderedArrayList.h"
+
+UnorderedArrayList::UnorderedArrayList() {
+    // Initialize the array and other variables
+    // You can set initial values
+}
+
+UnorderedArrayList::UnorderedArrayList(int initialCapacity) {
+    // Initialize the array with the specific initial capacity
+}
+
+bool UnorderedArrayList::add(int data) {
+    // Implement the logic to add data to the unordered array
+    // Return true if correct, false otherwise
+}
+
+bool UnorderedArrayList::add(int index, int data) {
+    // Implement the logic to add data at the specific index
+    // Return true if correct, false otherwise
+}
